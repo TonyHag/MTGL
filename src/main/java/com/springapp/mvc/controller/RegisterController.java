6367f6d1cdd7b221afc2e4281df7aad962f4e55f
skogland <skogland.eirik@gmail.com>
2014-12-02 20:34:47 +0100

    modifisert controller og view for registrering laget objekt for form data til registrering

--- src/main/java/com/springapp/mvc/controller/RegisterController.java
+++ src/main/java/com/springapp/mvc/controller/RegisterController.java
@@ -1,7 +1,9 @@
 package com.springapp.mvc.controller;
 
+import com.springapp.mvc.model.RegisterFormData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,15 +21,24 @@ public class RegisterController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String submitRegisterForm(ModelMap model) {
+    public String submitRegisterForm(ModelMap model, @ModelAttribute RegisterFormData data) {
 
-        // hent parametre
+
+        // RegisterFromData data blir laget automatisk siden attributtene i RegisterFormData har samme navn som i formen
+        String username = data.getUsername();
+        String password1 = data.getPassword1();
+        String password2 = data.getPassword2();
+        String email = data.getEmail();
+
+
 
         // valider parametre, metachars, lengde, osv
 
         // sjekk om brukernavn ledig
 
             // ledig
+                // krypter passord
+                // generer unik id
                 // opprett bruker
                 // lagre i database
                 // returner til login
